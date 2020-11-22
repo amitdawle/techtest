1ecb04a3616421b58b32b87503eba338ef63ec8a
Amit Dawle <amit.dawle@gmail.com>
2020-11-22 18:45:47 +0000

    Updated tests

--- src/main/java/com/db/dataplatform/techtest/server/api/controller/ServerController.java
+++ src/main/java/com/db/dataplatform/techtest/server/api/controller/ServerController.java
@@ -5,16 +5,22 @@ import com.db.dataplatform.techtest.server.component.Server;
 import com.db.dataplatform.techtest.server.persistence.BlockTypeEnum;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Pattern;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
@@ -38,20 +44,21 @@ public class ServerController {
 
 
     @GetMapping(value = "/data/{blockType}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<DataEnvelope>> getDataByBlockType(@PathVariable String blockType) throws IOException {
+    //@PathVariable(value = "type") QuestionCategory categor
+    public ResponseEntity<List<DataEnvelope>> getDataByBlockType(@PathVariable(value = "blockType") BlockTypeEnum blockType) throws IOException {
         Objects.requireNonNull(blockType);
         log.info("Request received to query data for block type {}", blockType);
-        List<DataEnvelope> data = server.findBy(BlockTypeEnum.valueOf(blockType));
+        List<DataEnvelope> data = server.findBy(blockType);
         return ResponseEntity.ok(data);
     }
 
 
     @PatchMapping(value = "/update/{name}/{newBlockType}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Boolean> updateBlockType(@PathVariable String name, @PathVariable String newBlockType) throws IOException {
+    public ResponseEntity<Boolean> updateBlockType(@PathVariable String name, @PathVariable(value = "newBlockType") BlockTypeEnum newBlockType) throws IOException {
         Objects.requireNonNull(name);
         Objects.requireNonNull(newBlockType);
         log.info("Request received to update block type of {} to block type {}", name, newBlockType);
-        boolean result = server.updateBlockType(name, newBlockType);
+        boolean result = server.updateBlockType(name, newBlockType.name());
         return ResponseEntity.ok(result);
     }
 
--- src/test/java/com/db/dataplatform/techtest/api/controller/ServerControllerComponentTest.java
+++ src/test/java/com/db/dataplatform/techtest/api/controller/ServerControllerComponentTest.java
@@ -2,9 +2,12 @@ package com.db.dataplatform.techtest.api.controller;
 
 import com.db.dataplatform.techtest.TestDataHelper;
 import com.db.dataplatform.techtest.server.api.controller.ServerController;
+import com.db.dataplatform.techtest.server.api.model.DataBody;
 import com.db.dataplatform.techtest.server.api.model.DataEnvelope;
-import com.db.dataplatform.techtest.server.exception.HadoopClientException;
+import com.db.dataplatform.techtest.server.api.model.DataHeader;
 import com.db.dataplatform.techtest.server.component.Server;
+import com.db.dataplatform.techtest.server.persistence.BlockTypeEnum;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,12 +22,14 @@ import org.springframework.web.util.UriTemplate;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
@@ -44,7 +49,7 @@ public class ServerControllerComponentTest {
 	private ServerController serverController;
 
 	@Before
-	public void setUp() throws HadoopClientException, NoSuchAlgorithmException, IOException {
+	public void setUp() throws NoSuchAlgorithmException, IOException {
 		serverController = new ServerController(serverMock);
 		mockMvc = standaloneSetup(serverController).build();
 		objectMapper = Jackson2ObjectMapperBuilder
@@ -58,7 +63,6 @@ public class ServerControllerComponentTest {
 
 	@Test
 	public void testPushDataPostCallWorksAsExpected() throws Exception {
-
 		String testDataEnvelopeJson = objectMapper.writeValueAsString(testDataEnvelope);
 
 		MvcResult mvcResult = mockMvc.perform(post(URI_PUSHDATA)
@@ -70,5 +74,54 @@ public class ServerControllerComponentTest {
 		boolean checksumPass = Boolean.parseBoolean(mvcResult.getResponse().getContentAsString());
 		assertThat(checksumPass).isTrue();
 	}
+
+	@Test
+	public void testPatchPostCallWorksAsExpected() throws Exception {
+
+		String testDataEnvelopeJson = objectMapper.writeValueAsString(testDataEnvelope);
+		when(serverMock.updateBlockType(anyString(), anyString())).thenReturn(true);
+
+		MvcResult mvcResult = mockMvc.perform(patch(URI_PATCHDATA.expand("name" , BlockTypeEnum.BLOCKTYPEA.name()))
+				.content(testDataEnvelopeJson)
+				.contentType(MediaType.TEXT_PLAIN))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		boolean response = Boolean.parseBoolean(mvcResult.getResponse().getContentAsString());
+		assertThat(response).isTrue();
+	}
+
+
+	@Test
+	public void testGetDataInvalidBlockType() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(get(URI_GETDATA.expand("ddd")))
+				.andExpect(status().is(400))
+				.andReturn();
+		assertThat(mvcResult.getResponse().getStatus()).isEqualTo(400);
+	}
+
+	@Test
+	public void testGetDataWorksAsExpected() throws Exception {
+		DataBody b = new DataBody("foo" ,"X");
+		DataHeader h = new DataHeader("A" , BlockTypeEnum.BLOCKTYPEA);
+		DataEnvelope e = new DataEnvelope(h, b);
+
+		when(serverMock.findBy(BlockTypeEnum.BLOCKTYPEA))
+				.thenReturn(Arrays.asList(e));
+		MvcResult mvcResult = mockMvc.perform(get(URI_GETDATA.expand(BlockTypeEnum.BLOCKTYPEA.name())))
+				.andExpect(status().is(200))
+				.andReturn();
+		List<DataEnvelope> envelope = objectMapper.readValue(mvcResult.getResponse().getContentAsString(),
+				new TypeReference<List<DataEnvelope>>() { });
+
+		assertThat(mvcResult.getResponse().getStatus()).isEqualTo(200);
+		assertThat(envelope.size()).isEqualTo(1);
+		assertThat(envelope.get(0).getDataBody().getDataBody()).isEqualTo("foo");
+		assertThat(envelope.get(0).getDataHeader().getName()).isEqualTo("A");
+		assertThat(envelope.get(0).getDataHeader().getBlockType()).isEqualTo(BlockTypeEnum.BLOCKTYPEA);
+
+	}
+
+
 
 }
